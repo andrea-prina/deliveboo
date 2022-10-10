85e6c2d356def19aa1b1133ba13baa33e2e831f1
bigeppo <carlog91cast@gmail.com>
2022-10-10 12:18:51 +0200

    Revert "fix: :bug:"
    This reverts commit 8e6500e06aa8ee44826c5e995370d01bec51cd2a.

--- public/js/front.js
+++ public/js/front.js
@@ -16255,7 +16255,7 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-module.exports = __webpack_require__(/*! /Users/stefano/deliveboo/resources/js/front.js */"./resources/js/front.js");
+module.exports = __webpack_require__(/*! E:\Git Repo\deliveboo\resources\js\front.js */"./resources/js/front.js");
 
 
 /***/ })
