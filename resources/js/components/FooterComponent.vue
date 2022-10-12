0971737a651cc9d4b73a01a5936881dead40b069
saff-it <saff_it@ymail.com>
2022-10-12 11:55:58 +0200

    feat: :art: Footer style and structure improvments

--- resources/js/components/FooterComponent.vue
+++ resources/js/components/FooterComponent.vue
@@ -1,17 +1,28 @@
 <template>
   <footer>
-    <div class="top-footer">
+    <div class="top-footer container">
         <div class="row">
-            <div class="col-3">
+            <div class="ms_footer-card col-3 text-white">
+                <ul>
+                    <h5>Scopri Deliveboo</h5>
+                    <li>Investitori</li>
+                    <li>Chi siamo</li>
+                    <li>Ristoranti</li>
+                    <li>Altro</li>
+                    <li>Pressroom</li>
+                    <li>Programmazione</li>
+                    <li>Design</li>
+                    <li>Lavora con noi</li>
+                    <li>Diventa nostro partner</li>
+                </ul>
+            </div>
+            <div class="ms_footer-card col-3 text-white">
                 ciao
             </div>
-            <div class="col-3">
+            <div class="ms_footer-card col-3 text-white">
                 ciao
             </div>
-            <div class="col-3">
-                ciao
-            </div>
-            <div class="col-3">
+            <div class="ms_footer-card col-3 text-white">
                 ciao
             </div>
         </div>
@@ -32,9 +43,23 @@ export default {
 </script>
 
 
-<style lang="scss">
-// @import "../../sass/generals.scss";
-// @import "../../sass/_variables.scss";
+<style lang="scss" scoped>
 @import "../../sass/app.scss";
+
+footer{
+    background-color: $footer-main;
+    .ms_footer-card{
+        background-color: #434848;
+        padding: 20px 8px;
+
+        ul{
+            list-style: none;
+
+        }
+    }
+
+}
+
+
 
 </style>
