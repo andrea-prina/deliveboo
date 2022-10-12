e5f0021468f8e6bf01f106bde7aaea85433208bd
bigeppo <carlog91cast@gmail.com>
2022-10-12 11:21:23 +0200

    feat: :art: add navbar in HeaderNav component with style

--- resources/js/components/HeaderNav.vue
+++ resources/js/components/HeaderNav.vue
@@ -1,15 +1,44 @@
 <template>
     <div>
-       
-        
-        
+        <nav class="navbar navbar-expand-lg navbar-light bg-light brand-title navbar-back">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="#">Deliveboo</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                    data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false"
+                    aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarNavDropdown">
+                    <ul class="navbar-nav">
+                        <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="http://127.0.0.1:8000/login">Login</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="http://127.0.0.1:8000/register">Register</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
     </div>
 </template>
 <script>
 export default {
-    
+    name: HeaderNav,
 }
 </script>
-<style>
-    
+<style lang="scss">
+@import "../../sass/app.scss";
+@import "../../sass/partials/headerBackOffice.scss";
+
+.navbar-back {
+    background-color: $brand-main;
+    color: $brand-white;
+
+    a {
+        color: $brand-white;
+
+    }
+
+}
 </style>
--- resources/js/pages/HomePage.vue
+++ resources/js/pages/HomePage.vue
@@ -1,3 +1,30 @@
+<template>
+    <div>
+        <HeaderNav />
+        <FooterComponent />
+    </div>
+</template>
+
+<script>
+import HeaderNav from '../components/HeaderNav.vue'
+import FooterComponent from '../components/FooterComponent.vue'
+
+export default {
+    name: 'HomePage',
+    components: {
+        HeaderNav,
+        FooterComponent,
+    }
+}
+</script>
+
+<style lang="scss">
+@import "../../sass/app.scss";
+</style>
+
+
+
+
 // Header con login, register (riprendere quello dal Blade) - COMPONENT
 // Jumbotron con ricerca (searchbar - COMPONENT)
 // Elenco ristoranti "spotlight" (cardHomepage - COMPONENT)
