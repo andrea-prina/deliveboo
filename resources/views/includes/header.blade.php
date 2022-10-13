56605ead9611060c5ca297fef99c493b9756d506
pyroYV <simonerizzo.pvt@gmail.com>
2022-10-13 15:27:50 +0200

    fix: :bug: Correct logo showing in backend header

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -1,7 +1,7 @@
 <nav class="navbar navbar-expand-md navbar-light shadow-sm brand-title navbar-back ">
     <div class="container">
         <a class="navbar-brand" href="{{ url('/') }}">
-            <img src="https://creativereview.imgix.net/content/uploads/2016/09/Deliveroo-Logo-Crop.png" alt="DeliveBoo" height="30">
+            <img src="../../../public/assets/img/logo-white.png" alt="DeliveBoo Logo" height="50">
            {{--  {{ config('app.name', 'DeliveBoo') }} --}}
         </a>
         <button class="navbar-toggler" type="button" data-toggle="collapse"
