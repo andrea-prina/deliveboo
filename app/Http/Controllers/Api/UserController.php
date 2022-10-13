f9d6c63b8d3b579055efbbcb7aeaabd1aa97c417
andrea-prina <and.prina@gmail.com>
2022-10-13 12:53:34 +0200

    feat(API): :sparkles: Create Users index API with filter options - changed types.name col name

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use App\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class UserController extends Controller
 {
@@ -13,19 +14,42 @@ class UserController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function index()
-    {
-        $users = User::All();
-        if($users){
+
+    
+    
+    public function index(Request $request){
+
+        $restaurantName = $request->query('name');
+        $restaurantType = $request->query('type');
+
+        $query = DB::table('users')
+                    ->join('type_user', 'users.id', '=', 'type_user.user_id')
+                    ->join('types', 'type_user.type_id', '=', 'types.id');
+        
+        if($restaurantName) {
+            $query->where('restaurant_name', 'LIKE', '%' . $restaurantName . '%');
+
+            if($restaurantType) {
+                $query->where('type_name', '=', $restaurantType);
+            }
+        
+        } else if ($restaurantType) {
+            $query->where('type_name', '=', $restaurantType);
+        }
+
+        $result= $query->select('users.restaurant_name', 'users.delivery_fee', 'users.free_delivery', 'users.image_path', 'types.type_name')->get();
+        
+        if($result){
             return response()->json([
                 'response' => true,
                 'results' => [
-                    'data' => $users
+                    'data' => $result
                 ]]);
         } else return response('', 404);
     }
 
-    
+
+
     public function show($id)
     {
         $user = User::find($id);
