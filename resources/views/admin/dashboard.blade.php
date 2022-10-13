56312852f733a38ee7f00dd42346a98a12d9e92b
pyroYV <simonerizzo.pvt@gmail.com>
2022-10-13 15:52:50 +0200

    feat: :sparkles: Further development on dashboard

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -4,24 +4,20 @@
 
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                    <div class="alert alert-success" role="alert">
-                        {{ session('status') }}
-                    </div>
-                    @endif
-
-                    {{ __('You are logged in!') }}
-                </div>
+        <div class="col-12 card">
+            <img src="{{$user->image_path}}" alt="You restourant image" class="w-100">
+            <div class="card-body">
+                <h1 class="card-title">{{$user->name}}</h1>
+                <p class="card-text">Your registered email is : {{$user->email}}</p>
+                <p class="card-text">{{$user->password}} 👀</p>
+                <p class="card-text"> Delivery fee : {{$user->delivery_fee}}</p>
+                <p class="card-text"> VAT: {{$user->vat_number}}</p>
+                <a href="{{route('')}}" class="btn btn-primary">Edit</a>
             </div>
+
         </div>
     </div>
 </div>
-
 
 
 @endsection
