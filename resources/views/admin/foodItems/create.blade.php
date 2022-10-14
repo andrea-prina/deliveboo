5816861f244f48fe6c90b88f17a21a4c4f918d1e
saff-it <saff_it@ymail.com>
2022-10-14 18:09:58 +0200

    feat: :art: edit and create structure updated

--- resources/views/admin/foodItems/create.blade.php
+++ resources/views/admin/foodItems/create.blade.php
@@ -5,7 +5,7 @@
 @section('content')
 
 
-<div class="container d-flex p-5 text-center">
+<div class="container d-flex p-5 bg-white rounded border-violet">
     <div class="row justify-content-center w-100">
         <div class="col-6">
 
@@ -16,38 +16,14 @@
                       <li>{{ $error }}</li>
                   @endforeach
               </ul>
-              @endif
+          @endif
 
             <form action="{{ route('admin.foodItems.store', $food->id) }}" method="POST">
                 @csrf
                 @method('POST')
-                <!-- Name input -->
-                <div class="form-outline mb-4">
-                  <input type="text" required max='255'id="form4Example1" class="form-control" name="name"/>
-                  <label class="form-label" for="form4Example1">Name *</label>
-                </div>
 
-                <!-- Email input -->
-                <div class="form-outline mb-4">
-                  <input type="text" required id="form4Example2" class="form-control" name="image_path" />
-                  <label class="form-label" for="form4Example2">Image *</label>
-                </div>
-
-                 <!-- Email input -->
-                 <div class="form-outline mb-4">
-                    <input type="text" required id="form4Example2" class="form-control" name="description" />
-                    <label class="form-label" for="form4Example2">Description *</label>
-                  </div>
-
-                <!-- Message input -->
-                <div class="form-outline mb-4">
-                    <input type="number" step="0.01" min="0.01" required id="form4Example2" class="form-control" name="price" />
-                    <label class="form-label" for="form4Example2">Price *</label>
-                  </div>
-
-
-                <!-- Submit button -->
-                <button type="submit" class="btn btn-primary btn-block mb-4">Send</button>
+                @include('admin.foodItems.includes.form')
+                
               </form>
         </div>
     </div>
