334b53a093278aeaf8f84e497cf83348ec77f995
saff-it <saff_it@ymail.com>
2022-10-14 09:28:59 +0200

    feat: :art: delivery fee added dynamically

--- resources/js/components/RestaurantCard.vue
+++ resources/js/components/RestaurantCard.vue
@@ -10,7 +10,7 @@
             <p class="card-text">Some quick example text to build on the card title and make up the bulk of the
                 card's
                 content.</p>
-            <span>Consegna: € 5.00</span>
+            <span>Consegna: € {{ restaurant.delivery_fee }}</span>
         </div>
     </div>
 
