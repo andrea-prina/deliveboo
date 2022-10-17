29d1949176fb7b7b0d40c532c614c428d37b91a3
pyroYV <simonerizzo.pvt@gmail.com>
2022-10-17 15:39:55 +0200

    fix: :bug: fixes to '*' 's color and 'col' in food show

--- resources/views/admin/foodItems/includes/form.blade.php
+++ resources/views/admin/foodItems/includes/form.blade.php
@@ -1,26 +1,25 @@
 
 <div class="form-outline mb-4">
-    <label class="form-label" for="name">Name *</label>
+    <label class="form-label" for="name">Name <span class="star">*</span></label>
     <input type="text" required max='255'id="name" class="form-control" name="name" />
 </div>
 
 
 <div class="form-outline mb-4">
-    <label class="form-label" for="description">Description *</label>
+    <label class="form-label" for="description">Description <span class="star">*</span></label>
     <input type="text" required id="description" class="form-control" name="description" />
 </div>
 
 <div class="form-outline mb-4">
-    <label class="form-label" for="price">Price *</label>
+    <label class="form-label" for="price">Price <span class="star">*</span></label>
     <input type="number" step="0.01" min="0.01" required id="price" class="form-control"
-        name="price" />
+        name="price" min="0" max="99.99"/>
 </div>
 
-{{-- TODO: to be fixed --}}
 <div class="form-outline mb-4">
-    <label class="form-label" for="image_path">Image *</label>
+    <label class="form-label" for="image_path">Image</label>
     <input class="form-control" type="file" id="image_path" name="image_path">
-    
+
 </div>
 
 
