878300812e2067f48b89cc6216eda2618b59ce41
saff-it <saff_it@ymail.com>
2022-10-19 15:26:29 +0200

    feat: :art: Restaurant Menu vue set up

---
+++ resources/js/pages/RestaurantMenu.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        <RestaurantMenuJumbo/>
+        <RestaurantMenuContainer/>
+        <FooterComponent/>
+    </div>
+</template>
+
+<script>
+import RestaurantMenuJumbo from '../components/RestaurantMenuJumbo.vue'
+import RestaurantMenuContainer from '../components/RestaurantMenuContainer.vue'
+import FooterComponent from '../components/FooterComponent.vue'
+
+export default {
+    name: 'RestaurantMenu',
+    components: {
+        FooterComponent,
+        RestaurantMenuJumbo,
+        RestaurantMenuContainer,
+
+    },
+}
+</script>
+
+<style lang="scss">
+@import "../../sass/app.scss";
+</style>
