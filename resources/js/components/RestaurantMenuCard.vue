280167225b69cae625c22105bd622628df409634
bigeppo <carlog91cast@gmail.com>
2022-10-19 18:05:57 +0200

    feat: :art: keep styling menu card

--- resources/js/components/RestaurantMenuCard.vue
+++ resources/js/components/RestaurantMenuCard.vue
@@ -1,17 +1,21 @@
 <template>
     <div class="card my-card" style="width: 18rem;">
         <div class="row">
-            <div class="col-4">
-                <img src="https://www.scattidigusto.it/wp-content/uploads/2018/04/carbonara-ricetta-spaghetti.jpg" class="card-img-top" alt="...">
-            </div>
-            <div class="col-8">
+            <div class="col-4 p-0">
                 <div class="card-body">
-                    <h5 class="card-title">Carbonara</h5>
-                    <p class="card-text">Descrizione: Some quick example text to build on the card title and make up the
+                    <img src="https://www.scattidigusto.it/wp-content/uploads/2018/04/carbonara-ricetta-spaghetti.jpg"
+                        class="card-img-top" alt="..."/>
+                </div>
+
+            </div>
+            <div class="col-8 p-0">
+                <div class="card-body">
+                    <h5 class="card-title mt-1">Carbonara</h5>
+                    <p class="card-text my-1">Descrizione: Some quick example text to build on the card title and make up the
                         bulk of
                         the card's content.</p>
-                    <p class="card-text">Prezzo:</p>
-                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                    <p class="card-text mt-1">Prezzo:</p>
+                    <a href="#" class="btn btn-primary my-1">Go somewhere</a>
                 </div>
             </div>
         </div>
@@ -39,5 +43,19 @@ export default {
     //box-shadow: rgba(0, 0, 0, 0.24) 0px 3px 8px !important;
     //transition: transform 0.2s ease-in-out !important;
 
+
+}
+
+img {
+    width: 100% !important;
+    margin: 4px !important;
+}
+
+p {
+    font-size: 10px !important;
+}
+
+a {
+    font-size: 10px !important;
 }
 </style>
