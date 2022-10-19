f06ff5f91555ab2377f12ff9b47e834d3175cef1
bigeppo <carlog91cast@gmail.com>
2022-10-19 17:24:57 +0200

    feat: :art: styling Menu Card

--- resources/js/components/RestaurantMenuCard.vue
+++ resources/js/components/RestaurantMenuCard.vue
@@ -1,5 +1,15 @@
 <template>
-    <div class="bg-white">questa è benzina</div>
+    <div class="card myCard" style="width: 18rem;">
+        <img src="..." class="card-img-top" alt="...">
+        <div class="card-body">
+            <h5 class="card-title">Carbonara</h5>
+            <p class="card-text">Descrizione: Some quick example text to build on the card title and make up the bulk of
+                the card's content.</p>
+            <p class="card-text">Ingredienti:</p>
+            <p class="card-text">Prezzo:</p>
+            <a href="#" class="btn btn-primary">Go somewhere</a>
+        </div>
+    </div>
 </template>
 
 <script>
@@ -15,5 +25,10 @@ export default {
 <style lang="scss">
 @import "../../sass/app.scss";
 
-
+.myCard {
+    background-color: #fafafa;
+    border-radius: 5px;
+    box-shadow: rgba(0, 0, 0, 0.24) 0px 3px 8px;
+    transition: transform 0.2s ease-in-out;
+}
 </style>
