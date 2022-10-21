45582aa81673d2722edd325520e86ad3ac6e2fa1
andrea-prina <and.prina@gmail.com>
2022-10-21 15:36:01 +0200

    refactor: :recycle: Refactor cart component to include methods for reusability

--- resources/js/components/RestaurantMenuComponents/RestaurantMenuCart.vue
+++ resources/js/components/RestaurantMenuComponents/RestaurantMenuCart.vue
@@ -8,7 +8,7 @@
                     </div>
                     <div class="card rounded mb-4 position-relative">
                         <div class="card-body p-4">
-                            <div class="position-absolute top-right" @click="$emit('resetCart')">
+                            <div class="position-absolute bottom-right" @click="resetCart()">
                                 <a href="#/" class="text-danger"><i class="fas fa-trash fa-lg"></i></a>
                             </div>
                             <table class="table my-2">
@@ -24,12 +24,12 @@
                                         <td>{{item.name}}</td>
                                         <td colspan="2" class="text-center">
                                             <div class="d-flex justify-content-between">
-                                                <button class="btn btn-link p-0 px-2" @click="$emit('removeItem',item.id)">
+                                                <button class="btn btn-link p-0 px-2" @click="removeItem(item.id)">
                                                     <i class="fas fa-minus"></i>
                                                 </button>
                                                 <span>{{item.quantity}}</span>
                                                 <button class="btn btn-link p-0 px-2"
-                                                    @click="$emit('addItem', item)">
+                                                    @click="addItem(item)">
                                                     <i class="fas fa-plus"></i>
                                                 </button>
                                             </div>
@@ -43,17 +43,16 @@
                                 </tbody>
                             </table>
                             <div class="fw-bolder fs-5 p-2">
-                                Total € {{ total }}
+                                Total € {{ getTotal(deliveryFee, freeDelivery) }}
                             </div>
                         </div>
                     </div>
 
-                    <div class="card">
+                    <div class="card" v-if="!checkout">
                         <div class="card-body text-center">
                             <router-link :to="{'name':'CheckoutPage', 'params': {'restaurantId' : restaurantId },}">
                             <button type="button" class="btn bg-brand btn-block btn-lg w-100">Proceed to Pay</button>
                             </router-link>
-
                         </div>
                     </div>
 
@@ -69,25 +68,125 @@ export default {
     name: 'RestaurantMenuCart',
     props: {
 
-        cart : Array,
-        storageKey : String,
-        total : Number,
+        item : Object,
+        click : Boolean,
+        checkout : Boolean,
         deliveryFee : Number,
         freeDelivery : Number,
         restaurantId : Number,
     },
+
+    data: function(){
+        return {
+            cart:[],
+            storageKey:'deliveboo',
+        }
+    },
+
     methods: {
 
-    }
+        // Initialize cart if there is none
+        initCart(){
+            const data = this.getCart()
+            if(data){
+            this.cart = data
+            }else{
+            this.syncCart()
+            }
+        },
 
+        // Sync local storage with cart variable
+        syncCart() {
+            const localCartStorage = JSON.stringify(this.cart)
+            localStorage.setItem(this.storageKey, localCartStorage)
+        },
+
+        // Get cart from localstorage and parse it to an object
+        getCart(){
+            const stringResult = localStorage.getItem(this.storageKey)
+            if(stringResult){
+                return JSON.parse(stringResult)
+            }else{
+                return null
+            }
+        },
+
+        // Return the object if the related id is in the cart
+        findById(id){
+            return this.cart.findIndex((item) => item.id == id)
+        },
+
+        // Add item to cart
+        addItem(item){
+
+            const itemIndex = this.findById(item.id)
+
+            if(itemIndex >= 0) {
+                this.cart[itemIndex].quantity++
+
+            } else {
+                // When you add an item to the cart add the "quantity" and "restaurantId" properties, then sync
+                // Quantity is defaulted to 1 and is incresed with further addItem() calls
+                this.cart.push({ quantity: 1, restaurantId : this.this.restaurantId, ...item })
+            }
+            this.syncCart()
+        },
+
+        // Reduce item quantity property in cart. Delete it if quantity = 0, then sync
+        removeItem(id){
+
+            const itemIndex = this.findById(id)
+            if(itemIndex === -1) return
+            this.cart[itemIndex].quantity--
+            if(this.cart[itemIndex].quantity<= 0){
+                this.cart.splice(itemIndex,1)
+            }
+            this.syncCart()
+        },
+
+        // Delete cart by assigning it an empty array, then sync
+        resetCart(){
+
+            if(window.confirm('Do you really want to empty your cart?')){
+                this.cart = []
+                this.syncCart()
+            }
+        },
+
+        // Return total value of items in cart (price*quantity sum)
+        getTotal(delivery_fee, free){
+
+            let total = this.cart.reduce((acc, item) => acc + item.price * item.quantity, 0)
+            if (!free){
+                total += delivery_fee;
+            }
+
+            total = Math.round((total + Number.EPSILON) * 100) / 100
+
+            return total;
+        },
+
+    },
+
+    mounted(){
+        this.initCart();
+    },
+
+    watch : {
+        click : function(){
+            this.addItem(this.item);
+        }
+    }
+    
 }
+
 </script>
 
 <style lang="scss">
 @import "../../../sass/app.scss";
 
-.top-right {
-    top: 0.5rem;
+.bottom-right {
+    bottom: 0.5rem;
     right: 0.5rem;
 }
 
--- resources/js/components/RestaurantMenuComponents/RestaurantMenuContainer.vue
+++ resources/js/components/RestaurantMenuComponents/RestaurantMenuContainer.vue
@@ -6,7 +6,7 @@
                     <div class="row g-5 my-4">
                     <div class="col-12 col-lg-6 my-2" v-for="menuItem in menuItems" :key="menuItem.id">
                         <RestaurantMenuCard :menuitem="menuItem"
-                        @addToCart='addItem'/>
+                        @addToCart='setItemToPass'/>
                     </div>
                 </div>
                 </div>
@@ -14,16 +14,12 @@
                 <div class="col-12 col-lg-4 ">
                     <div class="col-12">
                         <RestaurantMenuCart
-                        v-if="cart.length"
-                        :cart="cart"
                         :deliveryFee="restaurantInfo.delivery_fee"
                         :freeDelivery="restaurantInfo.free_delivery"
-                        :total="getTotal(restaurantInfo.delivery_fee, restaurantInfo.free_delivery)"
                         :restaurantId="restaurantInfo.id"
-                        @resetCart="resetCart"
-                        @removeItem="removeItem"
-                        @addItem="addItem"
-
+                        :item="menuItem"
+                        :click="click"
+                        :checkout="false"
                         />
                     </div>
                 </div>
@@ -49,92 +45,26 @@ export default {
         menuItems : Array,
         restaurantInfo : Object,
 
-
     },
 
-    data() {
+    data : function(){
+
         return {
-            cart:[],
-            storageKey:'deliveboo'
+            menuItem : {},
+            click : false,
         }
-
+            
     },
-    methods: {
-        //se l'utente ha un cerrello lo carica
-        initCart(){
-            const data = this.getCart()
-            if(data){
-            this.cart = data
-            }else{
-            this.syncCart()
-            }
-        },
 
-        syncCart() {
-            const localCartStorage = JSON.stringify(this.cart)
-            localStorage.setItem(this.storageKey, localCartStorage)
+    methods : { 
+        setItemToPass : function(id){
+            this.menuItem = id;
+            this.click = !(this.click);
         },
-
-        getCart(){
-            const stringResult= localStorage.getItem(this.storageKey)
-            if(stringResult){
-                return JSON.parse(stringResult)
-            }else{
-                return null
-            }
-        },
-
-        //ritorna l'oggetto se esiste nel carrello
-        findById(id){
-            return this.cart.findIndex((item) => item.id == id)
-        },
-
-        addItem(item){
-            console.log({aggiunge:item})
-            const itemIndex = this.findById(item.id)
-            if(itemIndex >= 0){
-                this.cart[itemIndex].quantity++
-            }else{
-                this.cart.push({ quantity: 1, ...item })
-            }
-            this.syncCart()
-        },
-        //diminuisce di uno la quantity del item all'indice fornito o lo rimuove se era l'ultimo elemento
-        removeItem(id){
-            console.log({rimuove:id})
-            const itemIndex = this.findById(id)
-            if(itemIndex === -1) return
-            this.cart[itemIndex].quantity--
-            if(this.cart[itemIndex].quantity<= 0){
-                this.cart.splice(itemIndex,1)
-            }
-            this.syncCart()
-        },
-        resetCart(){
-            console.log(this.cart)
-            if(window.confirm('Do you really want to empty your cart?')){
-                this.cart = []
-                this.syncCart()
-            }
-        },
-        //ritorna il totale del carrello
-        getTotal(delivery_fee, free){
-
-            let total = this.cart.reduce((acc, item) => acc + item.price * item.quantity, 0)
-            if (!free){
-                total += delivery_fee;
-            }
-
-            total = Math.round((total + Number.EPSILON) * 100) / 100
-
-            return total;
-        },
-
-    },
-    mounted() {
-        this.initCart();
     }
+
 }
+
 </script>
 
 <style lang="scss">
