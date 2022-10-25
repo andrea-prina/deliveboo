a1ace31817c005c078e58aa2d339d0a13a30362c
andrea-prina <and.prina@gmail.com>
2022-10-25 09:01:25 +0200

    fix: :bug: Fix disable checkout button and titles alignmenet

--- resources/js/components/RestaurantMenuComponents/RestaurantMenuContainer.vue
+++ resources/js/components/RestaurantMenuComponents/RestaurantMenuContainer.vue
@@ -4,18 +4,19 @@
             <div class="row py-4">
                 <div class="col-12 col-lg-8">
                     
-                    <h3 class="fw-normal mb-0 text-black">Shopping Cart</h3>
+                    <h3 class="fw-normal mb-0 text-black">Menu</h3>
                     
                     <div class="row g-5 my-4">
                         <div class="col-12 col-lg-6 my-2" v-for="menuItem in menuItems" :key="menuItem.id" v-if="menuItem.availability">
-                            <RestaurantMenuCard  :menuitem="menuItem"
+                            <RestaurantMenuCard
+                            :menuitem="menuItem"
                             @addToCart='setItemToPass'/>
                         </div>
                     </div>
                 </div>
                 
             <!-- cart side  -->
-                <div class="col-12 col-lg-4 py-5">
+                <div class="col-12 col-lg-4">
                     <div>
                         <RestaurantMenuCart
                         :deliveryFee="restaurantInfo.delivery_fee"
@@ -27,8 +28,8 @@
                         />
                     </div>
                     <div class="d-flex justify-content-center">
-                        <router-link :to="{'name':'CheckoutPage', 'params': {'restaurantId' : restaurantId , 'restaurantInfo' : restaurantInfo},}">
-                            <button type="button" class="btn bg-brand btn-block btn-lg">Checkout</button>
+                        <router-link :to="{'name':'CheckoutPage', 'params': {'restaurantId' : restaurantId , 'restaurantInfo' : restaurantInfo},} " :class="{ disabled: !isCartNotEmpty }">
+                            <button type="button" class="btn bg-brand btn-block btn-lg" :disabled="isCartNotEmpty === false">Checkout</button>
                         </router-link>
                     </div>
                 </div>
@@ -89,5 +90,8 @@ export default {
 <style lang="scss" scoped>
 @import "../../../sass/app.scss";
 
+.disabled {
+    pointer-events: none;
+}
 
 </style>
