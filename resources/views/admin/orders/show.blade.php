2f18cfc4fcd6951d00f4a17e05eeee643be3e7c1
andrea-prina <and.prina@gmail.com>
2022-10-26 15:54:21 +0200

    feat: :sparkles: Enlarge customer details font - reduce dishes one

--- resources/views/admin/orders/show.blade.php
+++ resources/views/admin/orders/show.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
-    <h3 class="text-center my-5">Order n.{{ $order->id }}</h3>
+    <h3 class="text-center my-5 fw-bold">Order n.{{ $order->id }}</h3>
     <div class="container text-center">
         
         <div class="d-flex justify-content-center p-2 my-5 ">
@@ -9,13 +9,13 @@
                 <div class="col-12 col-md-10 ">
                     <div>
                         <div>
-                            <h5 class="card-title fw-bold fs-4 mb-2 brand-main-color">Customer Details</h5>
-                            <p><strong>Full Name</strong><br>{{ $order->customer_firstName }} {{ $order->customer_lastName }}</p>
-                            <p><strong>Address</strong><br>{{ $order->customer_address }}</p>
-                            <p><strong>Email</strong><br>{{ $order->customer_email }}</p>
-                            <p><strong>Phone Number</strong><br>{{ $order->customer_phoneNumber }}</p>
+                            <h1 class="card-title fw-bold fs-4 mb-2 brand-main-color">Customer Details</h1>
+                            <h5><strong>Full Name</strong><br>{{ $order->customer_firstName }} {{ $order->customer_lastName }}</h5>
+                            <h5><strong>Address</strong><br>{{ $order->customer_address }}</h5>
+                            <h5><strong>Email</strong><br>{{ $order->customer_email }}</h5>
+                            <h5><strong>Phone Number</strong><br>{{ $order->customer_phoneNumber }}</h5>
                             @if ($order->additional_notes != '')
-                                <p><strong>Additional Notes</strong><br>{{ $order->additional_notes }}</p>
+                                <h5><strong>Additional Notes</strong><br>{{ $order->additional_notes }}</h5>
                             @else
 
                             @endif
@@ -26,7 +26,7 @@
         </div>
         
         
-        <h5 class="card-title fw-bold fs-4 brand-main-color">Dishes details</h5>
+        <h1 class="card-title fw-bold fs-4 brand-main-color">Dishes details</h1>
         @forelse ($order->food_items as $food)
             <div class="d-flex justify-content-center p-2 mb-2 ">
                 <div class="row justify-content-center w-100">
@@ -34,9 +34,9 @@
                         <div class="card border-violet">
                             <div class="card-body">
                                 <h5 class="card-title fw-bold fs-4">{{ $food->name }}</h5>
-                                <p class="card-text fs-5">{{ $food->description }}</p>
-                                <p class="card-text">Price: € <span class="fs-5 fw-bold">{{ $food->price }}</span></p>
-                                <p class="card-text">Quantity: <span class="fs-5 fw-bold">{{ $food->pivot->food_quantity }}</span></p>
+                                <p class="card-text">{{ $food->description }}</p>
+                                <p class="card-text">Price: € <span class="fw-bold">{{ $food->price }}</span></p>
+                                <p class="card-text">Quantity: <span class="fw-bold">{{ $food->pivot->food_quantity }}</span></p>
                             </div>
                         </div>
                     </div>
